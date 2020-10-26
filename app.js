8138c396111681a13ce0e40943a388a2b6dbb069
Denys Fesenko <dfesuk@gmail.com>
2020-10-26 19:06:50 +0200

    :construction: started new hw

---
+++ app.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('a[href^="#"]').on('click', function(event) {
+      event.preventDefault();
+      var sc = $(this).attr("href"),
+          dn = $(sc).offset().top;
+      $('html, body').animate({scrollTop: dn}, 500);
+    });
+  });
